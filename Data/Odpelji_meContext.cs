6b58fac2a2556f00c9a27ffb13ab9da8a81528be
Brina <bz.brina@gmail.com>
2025-03-12 14:27:09 +0100

    Add files via upload

---
+++ Data/Odpelji_meContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OpremiSe.Models;
+using OdpeljiMe.Models;
+
+namespace Odpelji_me.Data
+{
+    public class Odpelji_meContext : DbContext
+    {
+        public Odpelji_meContext (DbContextOptions<Odpelji_meContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OpremiSe.Models.Avtomobil> Avtomobil { get; set; } = default!;
+        public DbSet<OdpeljiMe.Models.Mnenje> Mnenje { get; set; } = default!;
+        public DbSet<OdpeljiMe.Models.Placilo> Placilo { get; set; } = default!;
+        public DbSet<OdpeljiMe.Models.Rezervacija> Rezervacija { get; set; } = default!;
+        public DbSet<OdpeljiMe.Models.Uporabnik> Uporabnik { get; set; } = default!;
+    }
+}
